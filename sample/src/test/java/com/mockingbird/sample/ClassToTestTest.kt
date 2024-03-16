30c8b392265f2dccfde7bc4fc5765f2065d90054
Anthony Restaino <anthonyrestaino11@gmail.com>
2024-03-16 08:57:04 -0400

    Add tests to cover failure cases

--- sample/src/test/java/com/mockingbird/sample/ClassToTestTest.kt
+++ sample/src/test/java/com/mockingbird/sample/ClassToTestTest.kt
@@ -31,6 +31,18 @@ class ClassToTestTest {
         }
     }
 
+    @Test(expected = IllegalStateException::class)
+    fun `act1 expected failure`() {
+        val classToTest = ClassToTest(interfaceToVerify1, interfaceToVerify2)
+
+        classToTest.act1()
+
+        verify(interfaceToVerify1, interfaceToVerify2) {
+            // The first invocation has 1 as its parameter
+            interfaceToVerify1.performAction1(2)
+        }
+    }
+
     @Test
     fun act2() {
         val classToTest = ClassToTest(interfaceToVerify1, interfaceToVerify2)
@@ -48,6 +60,18 @@ class ClassToTestTest {
         }
     }
 
+    @Test(expected = IllegalStateException::class)
+    fun `act2 expected failure`() {
+        val classToTest = ClassToTest(interfaceToVerify1, interfaceToVerify2)
+
+        classToTest.act2()
+
+        verify(interfaceToVerify1, interfaceToVerify2) {
+            // performAction2 was invoked first
+            interfaceToVerify1.performAction1(1)
+        }
+    }
+
     @Test
     fun act3() {
         val classToTest = ClassToTest(interfaceToVerify1, interfaceToVerify2)
@@ -60,6 +84,18 @@ class ClassToTestTest {
         }
     }
 
+    @Test(expected = IllegalStateException::class)
+    fun `act3  expected failure`() {
+        val classToTest = ClassToTest(interfaceToVerify1, interfaceToVerify2)
+
+        classToTest.act3()
+
+        verify(interfaceToVerify1, interfaceToVerify2) {
+            // Only invoked 2 times
+            interfaceToVerify1.times(3) { interfaceToVerify1.performAction1(1) }
+        }
+    }
+
     @Test
     fun act4() {
         val classToTest = ClassToTest(interfaceToVerify1, interfaceToVerify2)
@@ -68,5 +104,15 @@ class ClassToTestTest {
 
         interfaceToVerify1.verifyNoInvocations()
         interfaceToVerify2.verifyNoInvocations()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `act4 expected failure`() {
+        val classToTest = ClassToTest(interfaceToVerify1, interfaceToVerify2)
+
+        classToTest.act3()
+
+        // act3 function call actually has invocations
+        interfaceToVerify1.verifyNoInvocations()
     }
 }
