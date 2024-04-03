d53cf33176568b930038f7ff694acd2829913fe7
Anthony Restaino <anthonyrestaino11@gmail.com>
2024-04-02 20:37:43 -0400

    Initial setup for publishing

--- mockingbird/core/build.gradle.kts
+++ mockingbird/core/build.gradle.kts
@@ -1,3 +1,41 @@
 plugins {
     kotlin("jvm")
+    id("maven-publish")
+}
+
+publishing {
+    publications {
+        create("maven", MavenPublication::class.java) {
+            groupId = "com.anthonycr.mockingbird"
+            artifactId = "core"
+            version = "0.1"
+
+            from(components.findByName("java"))
+
+            pom {
+                name = "Mockingbird"
+                description = "A minimalist faking framework exclusively for verifying interactions"
+                url = "https://github.com/anthonycr/Mockingbird"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/license/mit"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "anthonycr"
+                        name = "Anthony Restaino"
+                        email = "dev@anthonycr.com"
+                        url = "https://github.com/anthonycr"
+                    }
+                }
+                scm {
+                    url = "https://github.com/anthonycr/Mockingbird"
+                    connection = "scm:git:git://github.com/anthonycr/Mockingbird.git"
+                    developerConnection = "scm:git:ssh://git@github.com:anthonycr/Mockingbird.git"
+                }
+            }
+        }
+    }
 }
