332f454633fd41e11cd70c0502d2b97daa960510
Anthony Restaino <anthonyrestaino11@gmail.com>
2024-04-09 22:14:30 -0400

    Simplify setup

--- mockingbird/processor/build.gradle.kts
+++ mockingbird/processor/build.gradle.kts
@@ -14,30 +14,5 @@ dependencies {
 }
 
 mavenPublishing {
-    coordinates("com.anthonycr.mockingbird", "processor", "0.1")
-
-    pom {
-        name = "Mockingbird"
-        description = "A minimalist faking framework exclusively for verifying interactions"
-        url = "https://github.com/anthonycr/Mockingbird"
-        licenses {
-            license {
-                name = "MIT License"
-                url = "https://opensource.org/license/mit"
-            }
-        }
-        developers {
-            developer {
-                id = "anthonycr"
-                name = "Anthony Restaino"
-                email = "dev@anthonycr.com"
-                url = "https://github.com/anthonycr"
-            }
-        }
-        scm {
-            url = "https://github.com/anthonycr/Mockingbird"
-            connection = "scm:git:git://github.com/anthonycr/Mockingbird.git"
-            developerConnection = "scm:git:ssh://git@github.com:anthonycr/Mockingbird.git"
-        }
-    }
+    coordinates(artifactId = "processor")
 }
