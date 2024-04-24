2b3582cf54cd61ec3c0dee69e4160c5af375afbf
Anthony Restaino <anthonyrestaino11@gmail.com>
2024-04-24 17:49:04 -0400

    Make the core library compatible with older jvm versions wi

--- mockingbird/core/build.gradle.kts
+++ mockingbird/core/build.gradle.kts
@@ -1,4 +1,6 @@
 import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
@@ -9,8 +11,15 @@ kotlin {
     jvmToolchain(libs.versions.jvm.get().toInt())
 }
 
+java.targetCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+tasks.withType<KotlinCompile> {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_11)
+}
+
 mavenPublishing {
     signAllPublications()
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
-    coordinates(artifactId =  "core")
+    coordinates(artifactId = "core")
 }
