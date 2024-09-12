f0f8f625e054e5ca46994673671fe7288771ddef
WhosNickDoglio <nicholasdoglio@gmail.com>
2024-09-12 09:07:18 -0500

    WIP Adding more KMP targets

--- mockingbird/core/build.gradle.kts
+++ mockingbird/core/build.gradle.kts
@@ -8,12 +8,39 @@ plugins {
 
 kotlin {
     jvmToolchain(libs.versions.jvm.get().toInt())
+
     jvm {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
             languageVersion.set(KotlinVersion.KOTLIN_2_0)
         }
     }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    watchosArm32()
+    watchosArm64()
+    watchosX64()
+    watchosSimulatorArm64()
+    tvosArm64()
+    tvosX64()
+    tvosSimulatorArm64()
+    macosX64()
+    macosArm64()
+    linuxArm64()
+    linuxX64()
+    mingwX64()
+//    js(IR) {
+//        moduleName = "core"
+//        browser()
+//    }
+//    @OptIn(ExperimentalWasmDsl::class)
+//    wasmJs {
+//        moduleName = "core"
+//        browser()
+//    }
+
+    applyDefaultHierarchyTemplate()
 }
 
 java {
