6e046487f4fea880620499f936a4daea26f69f6e
Anthony Restaino <anthonyrestaino11@gmail.com>
2024-09-14 09:42:56 -0400

    Add verifyPartial to allow partial verification of invocations

--- mockingbird/core/src/main/kotlin/com/anthonycr/mockingbird/core/Verifiable.kt
+++ mockingbird/core/src/main/kotlin/com/anthonycr/mockingbird/core/Verifiable.kt
@@ -40,6 +40,29 @@ inline fun verify(vararg verifiable: Any, block: () -> Unit) {
 }
 
 /**
+ * Verifies the [verifiable] and invokes the [block], in which to call functions on the [verifiable]
+ * that are expected to be called. Unlike [verify] not all invocations need to be verified.
+ *
+ * This function cannot be called within another [verify] block.
+ */
+inline fun verifyPartial(vararg verifiable: Any, block: () -> Unit) {
+    val verifiableList: List<Verifiable> = verifiable.map {
+        check(it is Verifiable) { MUST_BE_VERIFIABLE }
+        it
+    }
+
+    verifiableList.forEach {
+        check(!it._mockingbird_verifying) { "Do not call verify within another verify block" }
+        it._mockingbird_verifying = true
+    }
+    block()
+    verifiableList.forEach {
+        it._mockingbird_invocations.clear()
+        it._mockingbird_verifying = false
+    }
+}
+
+/**
  * Verifies that all expected function invocations have been verified. Useful if there are no
  * invocations expected for a certain verifiable.
  *
