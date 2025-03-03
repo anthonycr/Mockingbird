39563dabbd4b872438cd55df89d46a30fb23e57e
Anthony Restaino <anthonyrestaino11@gmail.com>
2025-03-02 20:57:25 -0500

    Determine error location and log the location in the KSP error log

--- mockingbird/processor/src/main/kotlin/com/anthonycr/mockingbird/processor/internal/errors.kt
+++ mockingbird/processor/src/main/kotlin/com/anthonycr/mockingbird/processor/internal/errors.kt
@@ -1,7 +1,9 @@
 package com.anthonycr.mockingbird.processor.internal
 
 import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.symbol.FileLocation
 import com.google.devtools.ksp.symbol.KSNode
+import com.google.devtools.ksp.symbol.NonExistLocation
 
 /**
  * Check the provided [condition] and only allow the items through that pass.
@@ -15,7 +17,12 @@ fun <T> Sequence<T>.check(
     val passed = condition(it)
 
     if (!passed) {
-        logger.error(message, node(it))
+        val realNode = node(it)
+        val actualMessage = when (val location = realNode.location) {
+            is FileLocation -> "$message: ${location.filePath}:${location.lineNumber}"
+            NonExistLocation -> message
+        }
+        logger.error(actualMessage, node(it))
     }
 
     passed
