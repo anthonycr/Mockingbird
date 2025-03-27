6c07e2cc1552d533b4d6a552e19405266243414a
Anthony Restaino <anthonyrestaino11@gmail.com>
2025-03-26 20:24:49 -0400

    Show a more detailed error message when fake is used without the ksp dependency being applied

--- mockingbird/core/src/commonMain/kotlin/com/anthonycr/mockingbird/core/Fakes.kt
+++ mockingbird/core/src/commonMain/kotlin/com/anthonycr/mockingbird/core/Fakes.kt
@@ -8,4 +8,11 @@ inline fun <reified T> fake(): T = fake(T::class.java)
 /**
  * Create a fake implementation of type [clazz].
  */
-fun <T> fake(clazz: Class<T>): T = fakeInternal(clazz)
+fun <T> fake(clazz: Class<T>): T = try {
+    fakeInternal(clazz)
+} catch (noClassDefFoundError: NoClassDefFoundError) {
+    throw RuntimeException(
+        "Generated code is missing. Please apply the KSP plugin and add `kspTest(com.anthonycr.mockingbird:processor:<latest_version>)` as a dependency.",
+        noClassDefFoundError
+    )
+}
