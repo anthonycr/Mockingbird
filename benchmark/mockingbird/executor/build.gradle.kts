6aea37aeccc366a6854c44abf86e3a9c97976632
Anthony Restaino <anthonyrestaino11@gmail.com>
2025-09-20 08:30:50 -0400

    Use compiler plugin for benchmark instead

--- benchmark/mockingbird/executor/build.gradle.kts
+++ benchmark/mockingbird/executor/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     alias(libs.plugins.ksp.plugin)
+    id("com.anthonycr.mockingbird.gradle-plugin")
 }
 
 kotlin {
@@ -11,7 +12,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
     testImplementation(project(":mockingbird:core"))
-    kspTest(project(":mockingbird:processor"))
+//    kspTest(project(":mockingbird:processor"))
     kspTest(project(":benchmark:mockingbird:generator"))
 }
 
