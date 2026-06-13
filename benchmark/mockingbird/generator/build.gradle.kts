a15a6c7f432a9f6c8f1fa255d940fb38e8fc35b4
Anthony Restaino <anthonyrestaino11@gmail.com>
2026-06-12 22:45:42 -0400

    Update minimum supported version to 2.2

--- benchmark/mockingbird/generator/build.gradle.kts
+++ benchmark/mockingbird/generator/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 kotlin {
     jvmToolchain(libs.versions.jvm.get().toInt())
     compilerOptions {
-        languageVersion.set(KotlinVersion.KOTLIN_2_0)
+        languageVersion.set(KotlinVersion.KOTLIN_2_2)
     }
 }
 
